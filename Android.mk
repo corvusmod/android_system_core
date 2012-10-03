38690bb14a1366bf8c0b448085f968a84f232533
CorvusMod <corvusmod@home>
2012-10-03 14:34:23 +0200

    Fix Cedar Libs
    Change-Id: I8a889c5392cc2a6f1879faa9e7fe4568b7a3db32

--- Android.mk
+++ Android.mk
@@ -15,8 +15,8 @@
 #
 LOCAL_PATH := $(my-dir)
 
+include $(call first-makefiles-under,$(LOCAL_PATH))
+
 ifeq ($(TARGET_BOARD_PLATFORM),exDroid)
 LOCAL_CFLAGS += -DALLWINNER 
 endif
-
-include $(call first-makefiles-under,$(LOCAL_PATH))
